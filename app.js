5ae7aeaf0db06350791bc87a1fe2275192073d1d
rivas.julio <juliorivasdev@gmail.com>
2023-10-10 10:22:14 -0300

    first commit

--- app.js
+++
@@ -1,6 +0,0 @@
-const conclusion = document.querySelector(".titulo_conclusion");
-const parrafos = document.querySelector(".conteneder_parrafos");
-
-conclusion.addEventListener("click", () => {
-  parrafos.classList.toggle("mostrar_parrafos");
-});
